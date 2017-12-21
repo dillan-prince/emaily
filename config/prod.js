d1c7701309177c1476b19d69ed7bc5ede53444a2
Dillan Prince <dillan.prince@buildasign.com>
2017-12-21 16:01:19 -0600

    set up prod environment

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoConnectionString: process.env.MONGO_CONNECTION_STRING,
+  cookieKey: process.env.COOKIE_KEY
+};
