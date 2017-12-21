05d3753f6ded76487933e2273a58cddb39b1720b
Dillan Prince <dillan.prince@buildasign.com>
2017-12-21 10:41:40 -0600

    Add/Update users

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Server-side syntax for importing dependencies
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./models/User");
 require("./services/passport");
 
+mongoose.connect(keys.mongoConnectionString);
 const app = express();
+
 require("./routes/authRoutes")(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
